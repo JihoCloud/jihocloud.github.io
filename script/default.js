7307b5a07ebca499ba0b03884d88d8b0869c03e5
JihoCloud <105723984+JihoCloud@users.noreply.github.com>
2023-02-07 21:57:24 +0900

    Update default.js

--- script/default.js
+++ script/default.js
@@ -19,7 +19,7 @@ function check() {
         landing.style.display = 'none';
         body.style.overflow = '';
         sub.style.visibility = 'visible';
-        window.scrollTo(0,0);
+        window.scrollTo(0,1);
     } else {
         alert('잘못된 날짜입니다');
         return;
